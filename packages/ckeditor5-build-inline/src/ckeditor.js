93e7ca41ba9192a50ad7648ee0d64f955cd49d00
horvath.patrik <horvath.patrik1997@gmail.com>
2021-02-26 16:29:26 +0100

    Added alignment module

--- packages/ckeditor5-build-inline/src/ckeditor.js
+++ packages/ckeditor5-build-inline/src/ckeditor.js
@@ -8,6 +8,7 @@ import InlineEditorBase from '@ckeditor/ckeditor5-editor-inline/src/inlineeditor
 
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
@@ -36,6 +37,7 @@ export default class InlineEditor extends InlineEditorBase {}
 InlineEditor.builtinPlugins = [
 	Essentials,
 	UploadAdapter,
+	Alignment,
 	Autoformat,
 	Bold,
 	Italic,
@@ -74,6 +76,8 @@ InlineEditor.defaultConfig = {
 			'|',
 			'indent',
 			'outdent',
+			'|',
+			'alignment',
 			'|',
 			'blockQuote',
 			'insertTable',
